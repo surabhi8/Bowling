7f508ad081f5ceb9f4739a01d241ccb4b0f4dff5
surabhi8 <surabhigupta2442@gmail.com>
2018-01-23 12:12:18 +0530

    Second test case passed

--- scoring-bowling-test.js
+++ scoring-bowling-test.js
@@ -1,8 +1,12 @@
 const calculateBowlingScore = require('./scoring-bowling.js');
 
 describe('Calculating bowling score', () => {
-  test('My bowling score is ', () => {
+  test('My bowling score for rolls [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6] is 90', () => {
     expect(calculateBowlingScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]))
       .toBe(90);
+  });
+  test('My bowling score for rolls [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 7,10] is 101', () => {
+    expect(calculateBowlingScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 7, 10]))
+      .toBe(101);
   });
 });
